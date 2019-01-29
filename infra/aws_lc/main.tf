612040c89658ff1e61c25060b5f16576fd19bada
Max Siegieda <max.siegieda@reconfigure.io>
2019-01-29 15:24:09 +0000

    don't specify an LC name, allow autofill

--- infra/aws_lc/main.tf
+++ infra/aws_lc/main.tf
@@ -4,7 +4,6 @@ variable security_group_ids {
 }
 
 resource "aws_launch_configuration" "pull_and_run_web_app" {
-  name          = "web_server"
   image_id      = "ami-012fd5eb46f56731f"
   instance_type = "t2.micro"
   iam_instance_profile = "${var.iam_instance_profile_name}" 
